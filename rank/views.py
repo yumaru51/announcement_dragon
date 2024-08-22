cf39e00da2144c19b4b927239639a86bad573c5b
yumaru51 <yumaru51@yahoo.co.jp>
2024-08-22 23:54:32 +0900

    詳細画面パネル式＆画像追加

--- rank/views.py
+++ rank/views.py
@@ -12,31 +12,26 @@ def top(request):
         }
     data = {
         'category_dict': category_dict,
+        'all_category1s':Category1.objects.all(),
+        'all_category2s':Category2.objects.all(),
         'message': 'データを出力しました'
     }
     return render(request, 'rank/top.html', data)
 
 
 def detail(request, category1, category2):
-    # data = {
-    #     'user_name': ['川内', ],
-    #     'カテゴリー1': [30, 25],
-    #     'カテゴリー2': ['東京都', '大阪府'],
-    #     '製品名': ['東京都', '大阪府'],
-    #     'メーカー': ['東京都', '大阪府'],
-    #     '': ['東京都', '大阪府'],
-    #     '': ['東京都', '大阪府'],
-    # }
+
+    rank_records = RankData.objects.get(user_name='yumaru51', category1=category1, category2=category2)
     data = {
-        'user_name': ['yumaru51'],
-        'カテゴリー1': [category1],
-        'カテゴリー2': [category2],
+        'user_name': 'yumaru51',
+        'カテゴリー1': category1,
+        'カテゴリー2': category2,
         '製品名': ['東京都', '大阪府'],
         'メーカー': ['東京都', '大阪府'],
-        '': ['東京都', '大阪府'],
-        '': ['東京都', '大阪府'],
+        'rank_records': rank_records,
     }
-    return render(request, 'rank/detail.html', {'data': data})
+    return render(request, 'rank/detail.html', data)
+    # return render(request, 'rank/detail.html', {'data': data})
 
     # category_dict = {}
     # for category_data in Category2.objects.all():
