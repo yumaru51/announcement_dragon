5d9d3f9f5d72b70161b66a2ef2ac2099eded4e90
y-kawauchi <y-kawauchi@iskweb.co.jp>
2024-08-26 16:28:44 +0900

    formsで入力、saveをはよ

--- rank/forms.py
+++ rank/forms.py
@@ -1,37 +1,43 @@
 from django import forms
-from .models import RankData
+from .models import Category1, Category2, RankData
 
 
-category1_list = [
-    ('1', '家電'),
-    ('2', 'ゲーム'),
-    ('3', 'ファッション'),
-]
-category2_list = [
-    ('1', 'ドライヤー'),
-    ('2', 'スピーカー'),
-    ('3', 'プロジェクター'),
+# list用意　外部参照でlist作成('value', 'text')形式
+category1_list = [('', '')]
+for category1 in Category1.objects.all().values_list('id', 'category1'):
+    category1_list.append(category1)
+category2_list = [('', '')]
+for category2 in Category2.objects.all().values_list('id', 'category2'):
+    category2_list.append(category2)
+
+
+level_list = [
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
 ]
 
 
 class RankDataForm(forms.ModelForm):
-    user_name = forms.ChoiceField(label='ユーザー名', widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), initial='')
+    user_name = forms.CharField(label='ユーザー名', widget=forms.TextInput(attrs={'class': 'form-control'}), initial='')
     category1 = forms.ChoiceField(label='カテゴリー1', choices=category1_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), initial='')
     category2 = forms.ChoiceField(label='カテゴリー2', choices=category2_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), initial='')
-    product_name = forms.MultipleChoiceField(label='変更対象', widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-inline_change_target', 'required': 'True'}), required=True)
-    # maker = forms.CharField(label='その他', widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
-    # price = forms.CharField(label='変更管理名称', widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
-    # durability = forms.CharField(label='変更内容概略', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 8, 'cols': 60}), required=True)
-    # portability = forms.ChoiceField(label='継続/一過性', choices=level_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), initial='')
-    # functionality = forms.CharField(label='リスク低減処置', widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
-    # repeat = forms.ChoiceField(label='安全面', choices=safety_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), initial='')
-    # category = forms.ChoiceField(label='品質面', choices=quality_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), initial='')
+    product_name = forms.CharField(label='製品名', widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
+    maker = forms.CharField(label='メーカー', widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+    price = forms.ChoiceField(label='価格', choices=level_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), required=True)
+    durability = forms.ChoiceField(label='耐久性', choices=level_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), required=True)
+    portability = forms.ChoiceField(label='携帯性', choices=level_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), required=True)
+    functionality = forms.ChoiceField(label='機能性', choices=level_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), required=True)
+    repeat = forms.ChoiceField(label='リピート', choices=level_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), required=True)
+    category = forms.CharField(label='商品紹介', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 8, 'cols': 60}), initial='')
     # registration_time = forms.DateField(label='変更希望日', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}), required=True)
     # update_time = forms.DateField(label='変更希望日', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}), required=True)
     # lost_flag = forms.DateField(label='変更希望日', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}), required=True)
 
     class Meta:
         model = RankData
-        fields = ['user_name', 'category1', 'category2', 'product_name']
+        fields = ['user_name', 'category1', 'category2', 'product_name', 'maker', 'price', 'durability', 'portability',
+                  'functionality', 'repeat', 'category']
         widgets = {}
-
