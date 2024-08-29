faafde1db93d1b813cc0e9c8c74c93d85d3179ae
y-kawauchi <y-kawauchi@iskweb.co.jp>
2024-08-29 16:44:10 +0900

    memo更新

--- rank/views.py
+++ rank/views.py
@@ -24,13 +24,11 @@ def top(request):
             }
         rankdatas = RankDataForm(initial=dict(
             user_name='yumaru51',
-        )),
+        ))
+
         data = {
             'category_dict': category_dict,
-            'all_category1s': Category1.objects.all(),
-            'all_category2s': Category2.objects.all(),
             'rankdatas': rankdatas,
-            'message': 'データを出力しました'
         }
     return render(request, 'rank/top.html', data)
 
@@ -40,24 +38,10 @@ def detail(request, category1, category2):
     rank_records = RankData.objects.get(user_name='yumaru51', category1=category1, category2=category2)
     data = {
         'user_name': 'yumaru51',
-        'カテゴリー1': category1,
-        'カテゴリー2': category2,
         '製品名': ['東京都', '大阪府'],
         'メーカー': ['東京都', '大阪府'],
         'rank_records': rank_records,
     }
     return render(request, 'rank/detail.html', data)
-    # return render(request, 'rank/detail.html', {'data': data})
 
-    # category_dict = {}
-    # for category_data in Category2.objects.all():
-    #     category_dict[str(category_data.id)] = {
-    #         'category1': category_data.category1,
-    #         'category2': category_data.category2,
-    #     }
-    # data = {
-    #     'category_dict': category_dict,
-    #     'message': 'データを出力しました'
-    # }
-    # return render(request, 'rank/detail.html', data)
 
