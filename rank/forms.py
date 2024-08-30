a0b706cc05d1abbeeab681ccd6c342b97a40469b
y-kawauchi <y-kawauchi@iskweb.co.jp>
2024-08-30 16:58:33 +0900

    ファイルアップロード更新

--- rank/forms.py
+++ rank/forms.py
@@ -2,15 +2,7 @@ from django import forms
 from .models import Category1, Category2, RankData
 
 
-# list用意　外部参照でlist作成('value', 'text')形式
-category1_list = []
-for category1 in Category1.objects.all().values_list('category1', 'category1'):
-    category1_list.append(category1)
-category2_list = []
-for category2 in Category2.objects.all().values_list('category2', 'category2'):
-    category2_list.append(category2)
-
-
+# リスト固定値（Modelから取る場合はinitに）
 level_list = [
     ('1', '1'),
     ('2', '2'),
@@ -22,16 +14,16 @@ level_list = [
 
 class RankDataForm(forms.ModelForm):
     user_name = forms.CharField(label='ユーザー名', widget=forms.TextInput(attrs={'class': 'form-control'}), initial='')
-    category1 = forms.ChoiceField(label='カテゴリー1', choices=category1_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), initial='')
-    category2 = forms.ChoiceField(label='カテゴリー2', choices=category2_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), initial='')
-    product_name = forms.CharField(label='製品名', widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
-    maker = forms.CharField(label='メーカー', widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+    category1 = forms.ChoiceField(label='カテゴリー1', choices=[], widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), initial='')
+    category2 = forms.ChoiceField(label='カテゴリー2', choices=[], widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), initial='')
+    product_name = forms.CharField(label='製品名', widget=forms.TextInput(attrs={'class': 'form-control'}), initial='WTW-3HS', required=True)
+    maker = forms.CharField(label='メーカー', widget=forms.TextInput(attrs={'class': 'form-control'}), initial='不動技研', required=False)
     price = forms.ChoiceField(label='価格', choices=level_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), required=True)
     durability = forms.ChoiceField(label='耐久性', choices=level_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), required=True)
     portability = forms.ChoiceField(label='携帯性', choices=level_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), required=True)
     functionality = forms.ChoiceField(label='機能性', choices=level_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), required=True)
     repeat = forms.ChoiceField(label='リピート', choices=level_list, widget=forms.Select(attrs={'size': 1, 'class': 'form-select'}), required=True)
-    category = forms.CharField(label='商品紹介', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 8, 'cols': 60}), initial='')
+    category = forms.CharField(label='商品紹介', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 8, 'cols': 60}), initial='', required=False)
     # registration_time = forms.DateField(label='変更希望日', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}), required=True)
     # update_time = forms.DateField(label='変更希望日', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}), required=True)
     # lost_flag = forms.DateField(label='変更希望日', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}), required=True)
@@ -43,11 +35,6 @@ class RankDataForm(forms.ModelForm):
         widgets = {}
 
     def __init__(self, *args, **kwargs):
-        category1_choices = kwargs.pop('category1_choices', None)
-        category2_choices = kwargs.pop('category2_choices', None)
         super(RankDataForm, self).__init__(*args, **kwargs)
-
-        if category1_choices:
-            self.fields['category1'].choices = category1_choices
-        if category2_choices:
-            self.fields['category2'].choices = category2_choices
+        self.fields['category1'].choices = Category1.objects.all().values_list('category1', 'category1')
+        self.fields['category2'].choices = Category2.objects.all().values_list('category2', 'category2')
--- rank/views.py
+++ rank/views.py
@@ -2,19 +2,24 @@ from django.shortcuts import render, redirect
 from django.db.models import Model  # Model情報を取得
 from rank.models import Category1, Category2, RankData
 from rank.forms import RankDataForm
-import os
+from django.contrib import messages
 
 
 def top(request):
     if request.method == 'POST':
-        form = RankDataForm(request.POST, request.FILES)
-        if form.is_valid():
+        rank_data_form = RankDataForm(request.POST, request.FILES)
+
+        # ファイルがアップロードされているかをチェック
+        if 'product_pict' not in request.FILES or not request.FILES['product_pict']:
+            # アラートメッセージを表示
+            messages.error(request, 'ファイルが選択されていません。')
+        elif rank_data_form.is_valid():
             obj = RankData.objects.create(id=None)
             request_form = RankDataForm(data=request.POST, instance=obj)
             request_form.save()
-            user_name = form.cleaned_data['user_name']
-            category1 = form.cleaned_data['category1']
-            category2 = form.cleaned_data['category2']
+            user_name = rank_data_form.cleaned_data['user_name']
+            category1 = rank_data_form.cleaned_data['category1']
+            category2 = rank_data_form.cleaned_data['category2']
 
             # ファイルが1件もない、または新規画面の時　アップロード不可
             if request.FILES.__len__() == 0:
@@ -22,13 +27,14 @@ def top(request):
             else:
                 product_pict = request.FILES['product_pict']
 
-                file_name = form.cleaned_data['product_name'] + ".jpg"
+                file_name = rank_data_form.cleaned_data['product_name'] + ".jpg"
                 upload_file_path = 'C:\\Users\\y-kawauchi\\PycharmProjects\\announcement_dragon\\static\\img\\'
                 with open(upload_file_path + file_name, 'wb+') as destination:
                     for chunk in product_pict.chunks():
                         destination.write(chunk)
+            return redirect('top')
         else:
-            print(form.errors)
+            print(rank_data_form.errors)
         return redirect('top')
     else:
         category_dict = {}
@@ -37,20 +43,12 @@ def top(request):
                 'category1': category_data.category1,
                 'category2': category_data.category2,
             }
-
-        category1_list = Category1.objects.all().values_list('category1', 'category1')
-        category2_list = Category2.objects.all().values_list('category2', 'category2')
-        rankdatas = RankDataForm(
+        rank_data_form = RankDataForm(
             initial=dict(user_name='yumaru51'),
-            category1_choices=category1_list,
-            category2_choices=category2_list,
         )
-        # rankdatas.fields['category1'].choices = category1_list
-        # rankdatas.fields['category2'].choices = category2_list
-
         data = {
             'category_dict': category_dict,
-            'rankdatas': rankdatas,
+            'rank_data_form': rank_data_form,
         }
     return render(request, 'rank/top.html', data)
 
